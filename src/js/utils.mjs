a6ee79e94007494596db2dcd0faf0aada5ccfc47
Hunter <hdblake333@gmail.com>
2023-05-02 21:33:37 -0500

    fixed syntax

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,7 +22,7 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 export function getParam(param) {
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-  return urlParams.get(param)
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
 }
