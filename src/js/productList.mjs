427e885b351082536ea2bd7d3bb107e2e2c88d7f
= <=>
2023-05-15 16:45:35 -0500

    moved renderList to utils and imported to prodList

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,4 +1,5 @@
 import {getData} from './productData.mjs'
+import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
     return `<li class="product-card">
@@ -11,28 +12,14 @@ function productCardTemplate(product) {
     <h2 class="card__name">${product.NameWithoutBrand}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
   </li>`;
-}   
+}
 
-function renderListWithTemplate(
-    templateFn,
-    parentElement,
-    list,
-    position = "afterbegin",
-    clear = true
-  ) {
-    if (clear) {
-      parentElement.innerHTML = "";
-    }
-    const htmlString = list.map(templateFn);
-    parentElement.insertAdjacentHTML(position, htmlString.join(""));
-  }
-
-  export default async function productList(selector, category) {
-    // get the element we will insert the list into from the selector
-    const el = document.querySelector(selector);
-    // get the list of products
-    const products = await getData(category);
-    console.log(products);
-    // render out the product list to the element
-    renderListWithTemplate(productCardTemplate, el, products);
-  }
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
+  const products = await getData(category);
+  console.log(products);
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, el, products);
+}
