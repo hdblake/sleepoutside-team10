d1265d5a4debc4b3fef106d0538360394a72c730
edw17003 <edw17003@byui.edu>
2023-05-24 21:19:01 -0600

    categorized product pages

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -16,7 +16,7 @@ function addToCart() {
 function renderProductDetails() {
     document.querySelector("#productName").innerText = product.Brand.Name;
     document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
-    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").src = product.Images.PrimaryLarge;
     document.querySelector("#productImage").alt = product.Name;
     document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
     document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
