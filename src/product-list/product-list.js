ff46c58f5ff522ee9d7f9eee1d2b3c5d2c04fbe6
Hunter <hdblake333@gmail.com>
2023-05-28 23:58:27 -0500

    changed file path

--- src/product-list/product-list.js
+++
@@ -1,7 +0,0 @@
-import productList from "../js/productList.mjs";
-import { getParam, loadHeaderFooter } from "../js/utils.mjs";
-
-loadHeaderFooter();
-
-const category = getParam("category");
-productList(".product-list", category);
