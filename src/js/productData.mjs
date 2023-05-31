0c56605fe5421c3fbe16fa14787dc2bee0a650b0
Hunter <hdblake333@gmail.com>
2023-05-30 19:02:50 -0500

    fixing link issue

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -9,13 +9,13 @@ function convertToJson(res) {
 }
 
 export async function getData(category) {
-  const response = await fetch(baseURL + `products/search/${category}`);
+  const response = await fetch(baseURL + `/products/search/${category}`);
   const data = await convertToJson(response);
   return data.Result;
 }
 
 export async function findProductById(id) {
-  const response = await fetch(baseURL + `product/${id}`);
+  const response = await fetch(baseURL + `/product/${id}`);
   const product = await convertToJson(response);
   return product.Result;
 }
